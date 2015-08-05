32d4b53d3e34300161974f6d5e08fa1576d6e502
bradschafer <brad@theschafergroup.com>
2015-08-05 11:03:29 -0700

    hopefully fixed chat

--- WebRTC/classic/src/view/chat/RoomController.js
+++ WebRTC/classic/src/view/chat/RoomController.js
@@ -34,13 +34,13 @@ Ext.define('WebRTC.view.chat.RoomController', {
     },
 
     chatReceived: function(chat){
-        var list = this.lookupReference('historylist');
-            // store = this.getViewModel().getStore('messages'),
+        var list = this.lookupReference('historylist'),
+             store = this.getViewModel().getStore('messages');
             // message = Ext.create('WebRTC.model.chat.Message',chat);
 
         // message.set({mine:false});
 
-        // store.add(chat);
+         store.add(chat);
         // store.sync();
         list.scrollBy(0, 999999, true);
 
