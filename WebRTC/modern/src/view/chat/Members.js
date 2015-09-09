7ac3147501612093986fac0e3cd1d2f8ba69b5bf
Maximiliano Fierro <elmasse@gmail.com>
2015-09-09 17:46:23 -0300

    members list fixed for modern

--- WebRTC/modern/src/view/chat/Members.js
+++ WebRTC/modern/src/view/chat/Members.js
@@ -1,23 +1,11 @@
 Ext.define('WebRTC.view.chat.Members', {
-    extend: 'Ext.Panel',
+    extend: 'Ext.dataview.List',
     xtype: 'chatmembers',
-    autoScroll: true,
-    bodyPadding: 10,
-    items:[{
-        xtype:'dataview',
-        bind:{
-            store: '{members}'
-        },
-        itemSelector: 'div.member-wrap',
-        itemTpl: [
-            '<tpl for=".">',
-            '<div style="margin-bottom: 10px;" class="member-wrap">',
-            '<span class="x-fa fa-user" title="{name}"> </span> {name}',
-            '<br/>',
-            '</div>',
-            '</tpl>'
-        ]
-    }]
 
-
+    itemSelector: 'div.member-wrap',
+    itemTpl: [
+        '<div style="margin-bottom: 10px;" class="member-wrap">',
+        '   <span class="x-fa fa-user" title="{name}"></span> {name}',
+        '</div>'
+    ]
 });
--- WebRTC/modern/src/view/chat/Room.js
+++ WebRTC/modern/src/view/chat/Room.js
@@ -18,23 +18,22 @@ Ext.define('WebRTC.view.chat.Room', {
         {
             layout: {
                 type: 'hbox',
-                vertical: false,
                 align: 'stretch'
             },
-            flex:3,
+            flex: 3,
             items:[
                 {
                     layout: {
-                        type: 'box',
-                        orient: 'vertical',
+                        type: 'vbox',
                         align: 'stretch'
                     },
-                    flex: 4,
+                    flex: 1,
                     items: [
                         {
                             xtype: 'chatinfo',
                             hidden: true
-                        }, {
+                        }, 
+                        {
                             xtype: 'chathistory',
                             reference: 'chathistory',
                             flex: 2
@@ -42,88 +41,84 @@ Ext.define('WebRTC.view.chat.Room', {
                     ]
                 },
                 {
-                    hidden: false,
                     layout: {
-                        type: 'box',
-                        orient: 'vertical',
+                        type: 'vbox',
                         align: 'stretch'
                     },
-                    flex: 1,
                     items: [
                         {
                             items:[
-                            {
-                                style: 'display: block; background-color:#eeeeee; background-image: url(https://static.opentok.com/webrtc/v2.6.0/images/rtc/audioonly-silhouette.svg); background-position: center bottom; background-repeat: no-repeat; background-size: auto 76%;',
-                                xtype: 'container',
-                                layout: 'fit',
-                                minHeight: 180,
-                                bind:{
-                                    hidden: '{!isWebRTCSupported}'
+                                {
+                                    style: 'display: block; background-color:#eeeeee; background-image: url(https://static.opentok.com/webrtc/v2.6.0/images/rtc/audioonly-silhouette.svg); background-position: center bottom; background-repeat: no-repeat; background-size: auto 76%;',
+                                    xtype: 'container',
+                                    layout: 'fit',
+                                    minHeight: 180,
+                                    bind:{
+                                        hidden: '{!isWebRTCSupported}'
+                                    },
+                                    reference: 'you'
                                 },
-                                reference: 'you'
-                            },{
-                                xtype: 'toolbar',
-                                docked: 'bottom',
-                                bind:{
-                                    hidden: '{!isWebRTCSupported}'
-                                },
-                                items: [
-                                    {
-                                        bind: {
-                                            disabled: '{inVideoCall}',
-                                            iconCls: '{audioCallIcon}'
-                                        },
-                                        listeners: {
-                                            tap: 'onAudioCallRoom'
-                                        }
+                                {
+                                    xtype: 'toolbar',
+                                    docked: 'bottom',
+                                    bind:{
+                                        hidden: '{!isWebRTCSupported}'
                                     },
-                                    {
-                                        bind: {
-                                            disabled: '{inAudioCall}',
-                                            iconCls: '{videoCallIcon}'
+                                    items: [
+                                        {
+                                            bind: {
+                                                disabled: '{inVideoCall}',
+                                                iconCls: '{audioCallIcon}'
+                                            },
+                                            listeners: {
+                                                tap: 'onAudioCallRoom'
+                                            }
                                         },
-                                        listeners: {
-                                            tap: 'onVideoCallRoom'
-                                        }
-                                    },
-                                    {
-                                        xtype: 'spacer'
-                                    },
-                                    {
-                                        iconCls: 'x-fa fa-eye',
-                                        bind: {
-                                            disabled: '{!inVideoCall}',
-                                            iconCls: '{videoToggleIcon}'
+                                        {
+                                            bind: {
+                                                disabled: '{inAudioCall}',
+                                                iconCls: '{videoCallIcon}'
+                                            },
+                                            listeners: {
+                                                tap: 'onVideoCallRoom'
+                                            }
                                         },
-                                        listeners: {
-                                            tap: 'onPublishVideoToggle'
-                                        }
-                                    }, {
-                                        iconCls: 'x-fa fa-microphone',
-                                        bind: {
-                                            disabled: '{isMicDisabled}',
-                                            iconCls: '{audioToggleIcon}'
+                                        {
+                                            xtype: 'spacer'
                                         },
-                                        listeners: {
-                                            tap: 'onPublishAudioToggle'
+                                        {
+                                            iconCls: 'x-fa fa-eye',
+                                            bind: {
+                                                disabled: '{!inVideoCall}',
+                                                iconCls: '{videoToggleIcon}'
+                                            },
+                                            listeners: {
+                                                tap: 'onPublishVideoToggle'
+                                            }
+                                        }, {
+                                            iconCls: 'x-fa fa-microphone',
+                                            bind: {
+                                                disabled: '{isMicDisabled}',
+                                                iconCls: '{audioToggleIcon}'
+                                            },
+                                            listeners: {
+                                                tap: 'onPublishAudioToggle'
+                                            }
                                         }
-                                    }
-                                ]
-                            }],
-                            bodyPadding: 6
-                        }, {
-                            title: 'Members',
-                            collapsable: true,
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'titlebar',
+                            title: 'Members'
+                        },
+                        {
                             xtype: 'chatmembers',
-                            iconCls: 'x-fa fa-group fa-lg',
-                            flex: 1
-                        }, {
-                            title: 'Files',
-                            hidden: true,
-                            reference: 'chatfiles',
-                            // xtype: 'chatattachments',
-                            iconCls: 'x-fa fa-paperclip',
-                            flex: 1
+                            flex: 1,
+                            bind:{
+                                store: '{members}'
+                            }
                         }
                     ]
                 }
@@ -137,7 +132,4 @@ Ext.define('WebRTC.view.chat.Room', {
             flex: 2
         }
     ]
-
-
-
 });
